faed4be54dac51e1460d1133c94ea5e13af0bdb1
RicardoMeneses28 <84602487+RicardoMeneses28@users.noreply.github.com>
2021-06-18 14:36:14 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -9,5 +9,10 @@ firebase.initializeApp({
     messagingSenderId: "158475602954",
     appId: "1:158475602954:web:09bb472fccd1ec80a663f3",
     measurementId: "G-EQNMZEWXB9"
-  });
+   
+  }
+        firebase.initializeApp(firebaseConfig);
+  firebase.analytics();                
+                      
+     );
 
