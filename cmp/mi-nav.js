25358e827f6ac019773585143fbd770926944580
RicardoMeneses28 <84602487+RicardoMeneses28@users.noreply.github.com>
2021-06-19 14:16:02 -0500

    Update mi-nav.js

--- cmp/mi-nav.js
+++ cmp/mi-nav.js
@@ -52,11 +52,11 @@ class MiNav extends HTMLElement {
         html += /* html */
           `<li>
             <a href=
-"pasatiempos.html">Pasatiempos</a>
+"calculadora.html">CALCULADORA</a>
           </li>
           <li>
             <a href=
-      "usuarios.html">Usuarios</a>
+      "usuarios.html">CLIENTES</a>
           </li>`;
       }
       this.ul.innerHTML += html;
