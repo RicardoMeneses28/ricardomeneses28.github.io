a6c61df3260cdfbc4e4a229b14bb5a43da64893b
RicardoMeneses28 <84602487+RicardoMeneses28@users.noreply.github.com>
2021-06-19 11:30:52 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,19 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyBOI9BuUu0jxgBJP7U6nTJl0S4opN81eOk",
+    authDomain: "fbwebauth28.firebaseapp.com",
+    projectId: "fbwebauth28",
+    storageBucket: "fbwebauth28.appspot.com",
+    messagingSenderId: "158475602954",
+    appId: "1:158475602954:web:09bb472fccd1ec80a663f3",
+    measurementId: "G-EQNMZEWXB9"
+
+});
